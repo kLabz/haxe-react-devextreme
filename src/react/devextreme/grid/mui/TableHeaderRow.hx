bd43efede2323c985d0366d05c1121dd0d1ad972
k <k@klabz.org>
2018-07-02 14:03:20 +0200

    Added mui TableHeaderRow (untyped)

---
+++ src/react/devextreme/grid/mui/TableHeaderRow.hx
@@ -0,0 +1,7 @@
+package react.devextreme.grid.mui;
+
+import react.ReactComponent;
+
+@:jsRequire('@devexpress/dx-react-grid-material-ui', 'TableHeaderRow')
+extern class TableHeaderRow extends ReactComponent {}
+
