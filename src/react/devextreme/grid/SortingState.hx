ed43a4fb5840cb0132bc2ff80d8920b284542fb8
k <k@klabz.org>
2018-08-28 16:35:49 +0200

    Added typings + doc for SortingState

--- src/react/devextreme/grid/SortingState.hx
+++ src/react/devextreme/grid/SortingState.hx
@@ -7,11 +7,45 @@ import react.ReactComponent;
 	var Desc = 'desc';
 }
 
+/** Describes the sorting applied to a column */
 typedef Sorting = {
+	/** Specifies a column's name to which the sorting is applied. */
 	var columnName:String;
+
+	/** Specifies a column's sorting order. */
 	var direction:SortingDirection;
 }
 
+/** Describes additional column properties that the plugin can handle. */
+typedef ColumnExtension = {
+	/** The name of a column to extend. */
+	var columnName:String;
+
+	/** Specifies whether sorting is enabled for a column. */
+	var sortingEnabled:Bool;
+}
+
+typedef SortingStateProps = {
+	/** Specifies the applied sorting. */
+	@:optional var sorting:Array<Sorting>;
+
+	/** Specifies initial sorting in the uncontrolled mode. */
+	@:optional var defaultSorting:Array<Sorting>;
+
+	/** Specifies whether sorting is enabled for all columns. */
+	@:optional var columnSortingEnabled:Bool;
+
+	/** Additional column properties that the plugin can handle. */
+	@:optional var columnExtensions:Array<ColumnExtension>;
+
+	/** Handles sorting changes. */
+	@:optional var onSortingChange:Array<Sorting>->Void;
+}
+
+/**
+	A plugin that manages the sorting state.
+	It controls the list of columns that participate in sorting.
+*/
 @:jsRequire('@devexpress/dx-react-grid', 'SortingState')
-extern class SortingState extends ReactComponent {}
+extern class SortingState extends ReactComponentOfProps<SortingStateProps> {}
 
