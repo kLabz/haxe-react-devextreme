dc507efde3887df8dd5c40a86282386d936cfa0f
k <k@klabz.org>
2018-08-28 17:25:44 +0200

    Toolbar (+ mui): added typings + doc

--- src/react/devextreme/grid/Toolbar.hx
+++ src/react/devextreme/grid/Toolbar.hx
@@ -1,7 +1,19 @@
 package react.devextreme.grid;
 
 import react.ReactComponent;
+import react.ReactNode;
 
+typedef ToolbarRootProps = {
+	/** A React node to be placed in the toolbar. */
+	@:optional var children:ReactFragment;
+}
+
+typedef ToolbarProps = {
+	/** A component that renders the toolbar root element. */
+	var rootComponent:ReactNodeOf<ToolbarRootProps>;
+}
+
+/** A plugin that renders the Grid toolbar. */
 @:jsRequire('@devexpress/dx-react-grid', 'Toolbar')
-extern class Toolbar extends ReactComponent {}
+extern class Toolbar extends ReactComponentOf<ToolbarProps> {}
 
--- src/react/devextreme/grid/mui/Toolbar.hx
+++ src/react/devextreme/grid/mui/Toolbar.hx
@@ -1,7 +1,22 @@
 package react.devextreme.grid.mui;
 
 import react.ReactComponent;
+import react.ReactNode;
 
+import react.devextreme.grid.Toolbar.ToolbarRootProps;
+
+typedef ToolbarProps = {
+	/** A component that renders the toolbar root element. */
+	@:optional var rootComponent:ReactNodeOf<ToolbarRootProps>;
+}
+
+/** A plugin that renders the Grid toolbar. */
 @:jsRequire('@devexpress/dx-react-grid-material-ui', 'Toolbar')
-extern class Toolbar extends ReactComponent {}
+extern class Toolbar extends ReactComponentOfProps<ToolbarProps> {
+	/** A component that renders the toolbar root element. */
+	public static inline var Root = ToolbarRoot;
+}
+
+@:jsRequire('@devexpress/dx-react-grid-material-ui', 'Toolbar.Root')
+extern class ToolbarRoot extends ReactComponent {}
 
