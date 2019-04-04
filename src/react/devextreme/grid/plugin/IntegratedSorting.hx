69cec19bfabc8ff5649d0a24650b9b6ef1fd9367
k <k@klabz.org>
2019-04-04 16:41:20 +0200

    0.3.1 - Fix IntegratedSorting's ColumnExtension

--- src/react/devextreme/grid/plugin/IntegratedSorting.hx
+++ src/react/devextreme/grid/plugin/IntegratedSorting.hx
@@ -1,11 +1,23 @@
 package react.devextreme.grid.plugin;
 
 import react.ReactComponent;
-import react.devextreme.grid.Table.TableColumnExtension;
 
 typedef IntegratedSortingProps = {
 	/** Additional column properties that the plugin can handle. */
-	@:optional var columnExtensions:Array<TableColumnExtension>;
+	@:optional var columnExtensions:Array<IntegratedSortingColumnExtension>;
+}
+
+typedef IntegratedSortingColumnExtension = {
+	/** The name of the column to extend. */
+	var columnName:String;
+
+	/**
+		A sort compare function.
+
+		See the Sorting guide for more information.
+		https://devexpress.github.io/devextreme-reactive/react/grid/docs/guides/sorting/#custom-sorting-algorithm
+	 */
+	@:optional var compare:Any->Any->Int;
 }
 
 /**
